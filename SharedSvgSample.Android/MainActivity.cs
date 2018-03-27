970f4c83f9199d2a1d9c8acc0d822ae086155ee0
Rendy Del Rosario <rendy.delrosario@gmail.com>
2018-03-27 02:47:51 -0400

    initial commit

---
+++ SharedSvgSample.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using FFImageLoading.Forms.Droid;
+using FFImageLoading.Svg.Forms;
+
+namespace SharedSvgSample.Droid
+{
+    [Activity(Label = "SharedSvgSample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            CachedImageRenderer.Init(true);
+            var ignore = typeof(SvgCachedImage);
+            LoadApplication(new App());
+        }
+    }
+}
+
